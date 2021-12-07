408ec153254990aca6533eee979616d420c50fb9
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-06 22:44:51 -0300

    Ínicio Curso JAVA DEVDOJO

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01TesteIDE.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+public class Aula01TesteIDE {
+    // Isso é um comentário de uma linha
+
+    /*
+        Comentário engloba
+        várias linhas
+     */
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
