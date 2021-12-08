27e1cd05107f6d22950ffb72af9919c5235b9a63
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-07 22:10:32 -0300

    Fim de operadores JAVA and, or, atribuição, +, /, % <>, ++, --

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,9 +2,61 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - / *
         int numero1 = 10;
         int numero2 = 20;
         double resultado = numero1 / (double) numero2;
         System.out.println(resultado);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < >  <= >=  =(atribuição)  ==(comparação)  !=(diferente)
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDeVinte = 10 != 20;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualQueVinte " + isDezIgualQueVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDeVinte " + isDezDiferenteDeVinte);
+
+        // && (AND)   || (or)   !()
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlayStationCinco = 5000F;
+
+        boolean isPlayStationCincoCompravel = valorTotalContaCorrente > valorPlayStationCinco || valorTotalContaPoupanca > valorPlayStationCinco;
+        System.out.println("isPlayStationCincoCompravel "+isPlayStationCincoCompravel);
+
+        // =  +=  -=  *=  /=  %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
+        System.out.println(contador2++);
     }
 }
