8681052fa363f2162c0e72e01e9a798c8d86e831
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-15 20:10:30 -0300

    Condicionais Exercise about Switch

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // Usando switch e dado os valores de 1 a 7, imprima se é dia útil ou final de semana
+        // Considerando 1 como Domingo
+        byte dia = 7;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                System.out.println("Final de semana");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                System.out.println("Dia útil");
+                break;
+            case 3:
+                System.out.println("Terça");
+                System.out.println("Dia útil");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                System.out.println("Dia útil");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                System.out.println("Dia útil");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                System.out.println("Dia útil");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                System.out.println("Final de semana");
+                break;
+            default:
+                System.out.println("Dia inserido inválido");
+        }
+    }
+}
