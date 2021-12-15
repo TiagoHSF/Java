9ec60c91c5411e854826f3d4cea9152b5afa86f4
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-15 19:58:22 -0300

    Condicionais SWITCH & Exercício sobre Imposto

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        double salarioAnual = 45000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorImposto;
+        if(salarioAnual <= 34712){
+            valorImposto = salarioAnual * primeiraFaixa;
+        } else if(salarioAnual >= 34712 && salarioAnual <= 68507){
+            valorImposto = salarioAnual * segundaFaixa;
+        } else {
+            valorImposto = salarioAnual * terceiraFaixa;
+        }
+        System.out.println(salarioAnual);
+    }
+}
