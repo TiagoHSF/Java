26a40a35997dc816bc6d6760a346c2423424c72c
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-20 21:49:30 -0300

    Laços de repetição While, do-while e for

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -2,22 +2,32 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais06 {
     public static void main(String[] args) {
-        byte dia = 7;
-        switch (dia){
+        byte dia = 6;
+        switch (dia) {
             case 1:
-                System.out.println("Final de Semana");
-            case 7:
                 System.out.println("Final de Semana");
                 break;
             case 2:
+                System.out.println("Dia útil");
+                break;
             case 3:
+                System.out.println("Dia útil");
+                break;
             case 4:
+                System.out.println("Dia útil");
+                break;
             case 5:
+                System.out.println("Dia útil");
+                break;
             case 6:
                 System.out.println("Dia útil");
                 break;
+            case 7:
+                System.out.println("Final de Semana");
+                break;
             default:
-                System.out.println("Dia inválido");
+                System.out.println("Dia inválido!");
+                break;
         }
     }
 }
