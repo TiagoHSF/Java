7a131093e3daf2c3b4c48573d865708a8645ddeb
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-20 22:21:42 -0300

    Break in first 25 numbers

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -2,6 +2,13 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturasDeRepeticao03 {
     public static void main(String[] args) {
-
+        // Imprima os primeiros 25 números de um dado valor. Por exemplo, 50.
+        int valorMaximo = 50;
+        for (int i = 0; i <= valorMaximo ; i++) {
+            if(i > 25){
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }
