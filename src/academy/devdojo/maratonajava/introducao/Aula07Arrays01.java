98527d9ae303fb1e8e0dc431305c0a1aa1ca61f6
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-20 23:44:00 -0300

    Arrays pt 01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 15;
+        idades[1] = 20;
+        idades[2] = 25;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
