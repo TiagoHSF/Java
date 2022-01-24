884cb3f1b0fd0c2bed7fd5f2ca0060058d8699cb
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-01-24 19:30:15 -0300

    Java class | Objects using class

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/domain/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain;
+
+public class Estudante {
+    public String nome = "Tiago";
+    public int idade;
+    public char sexo;
+}
--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/domain/Estudante.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.domain;
-
-public class Estudante {
-    public String nome;
-    public int idade;
-    public char sexo;
-}
