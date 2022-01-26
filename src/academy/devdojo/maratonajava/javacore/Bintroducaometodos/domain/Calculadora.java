b3effd8894150a37361b4facaf7dfc6160399c53
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-01-25 23:08:48 -0300

    Using methods to access an object

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10)
+    }
+
+    public void subtrairDoisNumeros() {
+        System.out.println(21 - 4)
+    }
+}
