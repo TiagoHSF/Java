75160d51c5ff072c37feef327f1f9c75a8dd8e77
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-01-31 21:55:43 -0300

    PSVM Syntax

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest05.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest05.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
 
 public class CalculadoraTest05 {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Calculadora calculadora = new Calculadora();
         int[] numeros = {1,2,3,4,5}
         calculadora.somaArray(numeros);
