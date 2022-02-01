695ced2e0558d407d55f5f9722620642e66683ea
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-01-31 21:42:07 -0300

    Reference this

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+public class EstudanteTest02 {
+    public static void main(String[] args){
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        
+        estudante01.nome = "Tiago";
+        estudante01.idade = 19;
+        estudante01.sexo = "M";
+
+        estudante01.nome = "Aline";
+        estudante01.idade = 17;
+        estudante01.sexo = "F";
+
+        estudante01.imprime();
+        estudante02.imprime();
+    }
+}
