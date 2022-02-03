992a4a1b821e5d12bda9c8260230906b27309e25
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-02-03 20:57:14 -0300

    Constructors pt.02

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.domain.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args){
-        Anime anime = new Anime("Aline", "TV", 12, "Ação");
-        Anime anime2 = new Anime()
+        Anime anime = new Anime("Aline", "TV", 12, "Ação", "IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
