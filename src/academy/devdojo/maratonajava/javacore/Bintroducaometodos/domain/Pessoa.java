b26b2a871941dc7de40cdc77728d634f853d5b6e
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-02-02 22:33:44 -0300

    Access modifier private, get and set

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Pessoa.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args){
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Tiago";
+        pessoa.idade = 19;
+    }
+}
