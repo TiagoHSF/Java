d3da80ecb381d8b2c2e67a06817f40a226d15a56
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-02-22 23:00:14 -0300

    Associação - arrays de objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.domain.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafu");
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2, jogador3};
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+    }
+}
