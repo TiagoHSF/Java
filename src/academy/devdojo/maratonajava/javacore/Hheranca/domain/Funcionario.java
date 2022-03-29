3af211c7cdd0932af96d7278666e63c3fee21ae1
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-03-28 23:39:13 -0300

    herança - super

--- src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
@@ -3,6 +3,11 @@ package academy.devdojo.maratonajava.javacore.Hheranca.domain;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public void imprime() {
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
